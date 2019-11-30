4e835efc6e4c651608590d1a9f1ae8c555cd590b
tehzwen <zachthm@gmail.com>
2019-11-30 08:28:28 -0700

    Fix face orders to enable backface culling

--- src/main.js
+++ src/main.js
@@ -340,6 +340,9 @@ function drawScene(gl, deltaTime, state) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+    gl.enable(gl.CULL_FACE);
+    gl.cullFace(gl.BACK);
+    gl.frontFace(gl.CCW);
     gl.clearDepth(1.0); // Clear everything
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
